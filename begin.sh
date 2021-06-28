9d187b6f065d759340eb84920a5468b29c8973c5
darkany <td.for121015@gmail.com>
2021-06-28 00:50:14 -0600

    Se agrega la configuración para uso de gunicorn

--- begin.sh
+++ begin.sh
@@ -1,36 +1,9 @@
 #!/bin/bash
 
-[[ -f "$1"  ]] || { echo "se espera como primer parametro un archivo .env"; exit 1; }
+sleep 15
 
-for linea in $(ccdecrypt -c "$1"); do
-  echo "export $linea"
-  export $linea
-done
+python3 manage.py makemigrations
+python3 manage.py migrate
 
-clear
-while :
-do
-  echo "Escoja una opción "
-  echo "1. Iniciar el servidor"
-  echo "2. Aplicar las migraciones: migrate"
-  echo "3. Crear nueva migración: makemigration"
-  echo "4. Salir del menú "
-  echo -n "Debes seleccionar solo opciones del [1 al 4]"
-  echo " "
-  read opcion
-  case $opcion in
-    1)
-      echo "Iniciando servidor...";
-      python3 manage.py runserver;;
-    2)
-      echo "Aplicando migraciones...";
-      python3 manage.py migrate;;
-    3)
-      echo "Creando nueva migración";
-      python3 manage.py makemigration;;
-    4)
-      break;;
-    *) echo "$REPLY Es una opción inválida";
-      ;;
-    esac
-   done
+gunicorn --bind :8000 credentialmanager.wsgi:application --reload
+
